ccda94e40d9476a23e365e8afee650c3731648ac
Birtalan Etele <etele.birtalan@gmail.com>
2024-11-11 18:08:26 +0200

    condition 2.0: guess = answer instead guess != answer

--- guestinggame.py
+++ guestinggame.py
@@ -3,7 +3,9 @@ answer = 5
 print("Please guess number between 1 and 10: ")
 guess = int(input())
 
-if guess != answer:
+if guess == answer:
+    print("You got it first time")
+else:
     if guess < answer:
         print("Please guess higher")
     else:
@@ -13,8 +15,6 @@ if guess != answer:
         print("Well done, you guessed it!")
     else:
         print("Sorry, you have not guessed correctly")
-else:
-    print("You got it first time")
 
 # if guess < answer:
 #     print("Please guess higher")
