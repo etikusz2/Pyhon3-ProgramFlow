063492c4f46bbe972c4ca3452befca04f31eb1d8
Birtalan Etele <etele.birtalan@gmail.com>
2024-11-12 18:30:01 +0200

    more ranges

--- ranges.py
+++ ranges.py
@@ -1,2 +1,8 @@
-for i in range(1, 20):
+for i in range(0, 10, 2):
     print("i is now {}".format(i))
+
+# all the numbers from 0 to 100 that are divisible by 7
+
+for i in range(0, 101):
+    if i % 7 == 0:
+        print(i)
