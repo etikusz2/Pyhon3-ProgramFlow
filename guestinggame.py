b7ad9b9db72969cbc264b1885b2ebb72a1543d98
Birtalan Etele <etele.birtalan@gmail.com>
2024-11-13 18:06:04 +0200

    the random module and import

--- guestinggame.py
+++ guestinggame.py
@@ -1,35 +1,21 @@
-answer = 5
+import random
 
-print("Please guess number between 1 and 10: ")
+highest = 10
+answer = random.randint(1, highest)
+
+print("Please guess number between 1 and {}: ".format(highest))
 guess = int(input())
 
 if guess == answer:
     print("You got it first time")
-else:
+
+while guess != answer:
     if guess < answer:
         print("Please guess higher")
     else:
         print("Please guess lower")
     guess = int(input())
+    if guess == 0:
+        break
     if guess == answer:
         print("Well done, you guessed it!")
-    else:
-        print("Sorry, you have not guessed correctly")
-
-# if guess < answer:
-#     print("Please guess higher")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you guessed it!")
-#     else:
-#         print("Sorry, you have not guessed correctly")
-# elif guess > answer:
-#     print("Please guess lower")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you guessed it!")
-#     else:
-#         print("Sorry, you have not guessed correctly")
-#
-# else:
-#     print("You got it first time")
