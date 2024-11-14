b022fe99503ab26a871cfd55a2c5b820cd2d7714
Birtalan Etele <etele.birtalan@gmail.com>
2024-11-14 18:10:29 +0200

    else in the Hi Lo game

--- hilo.py
+++ hilo.py
@@ -5,11 +5,11 @@ print("Please think of a number between {} and {}".format(low, high))
 input("Press ENTER to start!")
 
 guesses = 1
-while True:
+while low != high:
     guess = low + (high - low) // 2
     high_low = input("My guess is {}. Should I guess higher or lower?"
                      " Enter h or l, or c if my guess was correct"
-                     .format(guesses)).casefold()
+                     .format(guess)).casefold()
 
     if high_low == "h":
         low = guess + 1
@@ -21,4 +21,7 @@ while True:
     else:
         print("Please enter h, l or c")
 
-    guesses =+ 1
+    guesses += 1
+else:
+    print("You thought of the number {}".format(low))
+    print("I got it in {} guesses".format(guesses))
